7f8c5fa6571e2bcd6b4252ef2198a83f952e1eb2
Sabid Rahman <136883738+sabid02@users.noreply.github.com>
2023-11-25 15:43:10 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div>
         <h1>This is home page</h1>
+        <h2>sabid</h2>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
