94f5d9a51a2bc3e9e753faff56f77ca3eac3abb2
Nasirul-Islam <nasirulislam866@gmail.com>
2023-11-26 00:13:27 +0600

    add editors

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
     <>
       <div>
         <h1>This is home page</h1>
+        <h2>Creators: </h2>
+        <ul>
+          <li>Nasirul Islam</li>
+          <li>Sabid Rahman</li>
+          <li>Rakibul Islam</li>
+        </ul>
       </div>
     </>
   )
